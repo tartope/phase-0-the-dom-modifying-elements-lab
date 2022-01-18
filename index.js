27df95579d32b250236ef2f19172b7c94994735e
Tunisia Artope <artope@yahoo.com>
2022-01-18 16:31:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+main.remove()
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+
+newHeader.id = 'victory'
+victory.innerHTML = "<h1>Tunisia is the champion</h1>";
