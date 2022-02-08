bb3114480453ffd3108e690046a23f8c99857188
Tunisia Artope <artope@yahoo.com>
2022-02-08 10:33:15 -0500

    commit tree b2fa53e57e41b68ee806a38cb049f833919c92f1

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 // Write your code here!
+// use this method to remove elements from the DOM.
 main.remove()
 
+//create elements with both lines 6 and 7.
 const newHeader = document.createElement('h1');
 document.body.appendChild(newHeader);
 
-newHeader.id = 'victory'
-victory.innerHTML = "<h1>Tunisia is the champion</h1>";
+//create id (or class) with lines 10.
+newHeader.id = 'victory';
+
+//add text with line 13.
+newHeader.textContent = 'Tunisia is the champion';
+
+
+
+
+
+
+// main.remove()
+
+// const newHeader = document.createElement('h1');
+// document.body.appendChild(newHeader);
+
+// newHeader.id = 'victory'
+// victory.innerHTML = "<h1>Tunisia is the champion</h1>";
